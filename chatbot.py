5548f0a477609e1ccafb1a6eda3f9ee8fb8d1268
Roman <r.o.feshchuk@student.khai.edu>
2021-11-09 16:40:27 +0300

    Chatbot (stage 2)

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,6 @@
 
+print("Hello! My name is Firstbot.")
+print("I was created in 2021")
+print("Please, remind me your name.")
+str_name = str(input())
+print("What a great name you have, " + str_name +"!")
--- hangman.py
+++ hangman.py
@@ -1,1 +1,116 @@
+from random import choice
 
+HANGMAN = (
+    """
+     ------
+     | |
+     |
+     |
+     |
+     |
+     |
+    ----------
+    """,
+    """
+     ------
+     | |
+     | O
+     |
+     |
+     |
+     |
+    ----------
+    """,
+    """
+     ------
+     | |
+     | O
+     | |
+     |
+     |
+     |
+    ----------
+    """,
+    """
+     ------
+     | |
+     | O
+     | /|
+     |
+     |
+     |
+    ----------
+    """,
+    """
+     ------
+     | |
+     | O
+     | /|\\
+     |
+     |
+     |
+    ----------
+    """,
+    """
+     ------
+     | |
+     | O
+     | /|\\
+     | /
+     |
+     |
+    ----------
+    """,
+    """
+     ------
+     | |
+     | O
+     | /|\\
+     | / \\
+     |
+     |
+    ----------
+    """
+)
+
+max_wrong = len(HANGMAN)
+WORDS = ('питон', 'игра', 'программирование', 'колесо', "танк", "мишь")
+word = choice(WORDS)
+so_far = '_' * len(word)
+wrong = 0
+used = []
+
+while wrong < max_wrong and so_far != word:
+    print(HANGMAN[wrong])
+    print('\nВы использовали следующие буквы:\n', used)
+    print('\nНа данный момент слово выглядит так:\n', so_far)
+
+    guess = input('\nВведите свой вариант: ')
+
+    while guess in used:
+        print('Вы уже угадали букву', guess)
+        guess = input('Введите своё предположение: ')
+
+    used.append(guess)
+
+    if guess in word:
+        print('\nДа! \'' + guess + '\' эта буква есть в слове!')
+
+        new = ''
+        for i in range(len(word)):
+            if guess == word[i]:
+                new += guess
+            else:
+                new += so_far[i]
+        so_far = new
+    else:
+        print('\nИзвините, данной буквы \'' + guess + '\' нет в слове.')
+        wrong += 1
+
+if wrong == max_wrong:
+    print(HANGMAN[wrong])
+    print('\nТебя повесили!')
+else:
+    print('\nПоздравляем Вы угадали слово!')
+
+print('\nЗагаданное слово было \'' + word + '\'')
