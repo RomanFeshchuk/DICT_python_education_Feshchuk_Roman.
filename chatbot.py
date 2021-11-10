35d35fe8252b4f2c305b161589594e4407e90c76
Roman <r.o.feshchuk@student.khai.edu>
2021-11-10 12:25:03 +0300

    ChatBot (5-th) stage

--- chatbot.py
+++ chatbot.py
@@ -21,3 +21,20 @@ while x <= str_num:
     print("" + str(x) + "!")
     x += 1
 print("Completed, have a nice day!")
+# stage 5
+print("How many file extensions does python have?")
+print("""
+1.Python has only 5 types of file extensions
+2.Python has only 9 types of file extensions
+3.Python has only 11 types of file extensions
+4.Python has only 12 types of file extensions
+""")
+def answers(new_answer):
+    answer = int(input())
+    if answer == 2:
+        print("Congratulations, have a nice day!")
+    else:
+        print("Wrong answer! Please, try again.")
+        answers("1")
+answers("3")
+answers("4")
