3806c200a03fb7d91e8b473eee117669f2ed72a6
Roman <r.o.feshchuk@student.khai.edu>
2021-11-21 14:37:15 +0300

    SecondStage

--- tictactoe.py
+++ tictactoe.py
@@ -3,3 +3,24 @@ cx = c.count("x")
 co = c.count("o")
 c_ = c.count("_")
 win = 0
+
+
+#The game board
+def play():
+    print("---------")
+    print("| ", end="")
+    for a in c[:3]:
+        print(a, end=" ")
+    print("|")
+    print("| ", end="")
+    for a in c[3:6]:
+        print(a, end=" ")
+    print("|")
+    print("| ", end="")
+    for a in c[6:9]:
+        print(a, end=" ")
+    print("|")
+    print("---------")
+
+
+
