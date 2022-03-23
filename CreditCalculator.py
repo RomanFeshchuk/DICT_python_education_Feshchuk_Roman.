0f8287994d934ace2302d76edf48a07c9d6c093a
Roman <r.o.feshchuk@student.khai.edu>
2022-03-23 20:21:43 +0300

    The First Stage

--- CreditCalculator.py
+++ CreditCalculator.py
@@ -3,7 +3,7 @@ def main ():
    print('Loan principal: 1000')
    print('The First month: repaid 250')
    print('The Second month: repaid 250')
-   print('The Thirg month: repaid 500')
+   print('The Third month: repaid 500')
    print('The Final output: The loan has been repaid!')
 if __name__ == '__main__':
     main()
